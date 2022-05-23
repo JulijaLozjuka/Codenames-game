126b65c23c6424e917a2b91c2fa31fcbca69c5a0
Julija Lozjuka <lozjuka.julija@gmail.com>
2022-05-23 12:40:43 +0300

    first commit

---
+++ src/main/java/lv/bootcamp/codenames/codenamesgame/CodenamesGameApplication.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.codenames.codenamesgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenamesGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenamesGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/bootcamp/codenames/codenamesgame/CodenamesGameApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.codenames.codenamesgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenamesGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
